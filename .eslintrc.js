244fd2b1fea4dfda88af79a21aeb03189b7c368d
RaffoMatto <rafaelbibianomatta@gmail.com>
2022-10-02 11:30:08 -0300

    Configurando workspace.

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard',
+    'prettier'
+  ],
+	plugins: ['vue', 'prettier'],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'prettier/prettier': 'error'
+  }
+}
+
+// module.exports = {
+//   root: true,
+//   extends: '@react-native-community',
+// };
---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  useTabs: false
+}
+
+
+// module.exports = {
+//   arrowParens: 'avoid',
+//   bracketSameLine: true,
+//   bracketSpacing: false,
+//   singleQuote: true,
+//   trailingComma: 'all',
+  
+// };
